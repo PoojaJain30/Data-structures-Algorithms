d1c8a9a105aee02aa22d4c7459878d94b6abe25a
Pooja <34033328+PoojaJain30@users.noreply.github.com>
2020-06-19 21:46:20 -0500

    Update Merge-Sorted-Array.py

---
+++ Arrays/Merge-Sorted-Array.py
@@ -0,0 +1,27 @@
+# Merge two sorted arrays 
+
+#using inbuild list methods
+def merge_sorted_arrays(arr1,arr2):
+    sorted_array = (arr1+arr2)
+    sorted_array.sort()
+    return sorted_array
+
+
+# using traditional method BigO = O(a+b)
+def merge_sorted_arrays1(arr1,arr2):
+    if len(arr1) == 0 or len(arr2) == 0:
+        return arr1+arr2
+    merge_arr = []
+    i = 0
+    j = 0
+    while(i < len(arr1) and  j <len(arr2)):
+        if arr1[i] <= arr2[j]:
+            merge_arr.append(arr1[i])
+            i +=1
+        elif arr2[j] < arr1[i]:
+            merge_arr.append(arr2[j])
+            j+=1
+    return merge_arr+arr1[i:]+arr2[j:]
+        
+
+print(merge_sorted_arrays1([0,3,4,31], []))
