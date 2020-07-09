f03c70343042d7aad154e091c08280a85a85ef5d
Pooja <34033328+PoojaJain30@users.noreply.github.com>
2020-07-08 22:27:46 -0500

    Update Selectionsort.py

---
+++ Sorting/Selectionsort.py
@@ -0,0 +1,15 @@
+#implement selection sort
+numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+
+def selection_sort(num):
+    for i in range(len(num)):
+        small = i
+        temp = num[i]
+        for j in range(i+1,len(num)):
+            if num[j] < num[small]:
+                small = j       
+        num[i] = num[small]
+        num[small] = temp  
+    print(num)
+
+selection_sort(numbers)
